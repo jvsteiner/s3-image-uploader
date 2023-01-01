abe0679772e37f59c660a3ac7fad357488811ff0
Jamie Steiner <jvsteiner@gmail.com>
2023-01-02 00:48:20 +0100

    added support for drag and drop, yaml tag settings for per document granularity, fixed path bug, updated README, changed frontmatter tag for drop support, added per-note folder option, formatted code

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 This is a plugin for Obsidian (https://obsidian.md). It was generated based on the [standard plugin template](https://github.com/obsidianmd/obsidian-sample-plugin).
 
-This project implements an image uploader, similar to others offerred by the community, with one important difference: you can provide your own s3 based storage, instead of relying on a third party service, such as `imgur`.
+This project implements an image uploader, similar to others offered by the community, with one important difference: you can provide your own s3 based storage, instead of relying on a third party service, such as `imgur`.
 
 Note: this plugin is still in development, and there maybe some bugs. Please report any issues you find.
 
-It was inspired by the following plugins:
+It was inspired by the awesome Markdown editor, Typora, and the following Obsidian plugins:
 
 -   [Obsidian Image Uploader](https://github.com/Creling/obsidian-image-uploader)
 -   [Remotely Save](https://github.com/remotely-save/remotely-save)
@@ -16,11 +16,11 @@ It was inspired by the following plugins:
 
 You have to set up your own s3 bucket, and provide the following information to the plugin:
 
--   `bucket`: the name of your bucket
+-   `bucket`: the name of your bucket (must already exist)
 -   `region`: the region of your bucket
 -   `accessKeyId`: the access key id for an s3 user with write access to your bucket
 -   `secretAccessKey`: the secret access key for the s3 user
--   `folder`: the folder in your bucket where you want to store the images (optional)
+-   `folder`: the folder in your bucket where you want to store the images (optional, and will be created on the fly if it does not exist.)
 
 If you want others to be able to view the images, you need to make your bucket world readable. You can do this by adding the following policy to your bucket:
 
@@ -39,7 +39,7 @@ If you want others to be able to view the images, you need to make your bucket w
 }
 ```
 
-You also need to set up a CORS policy:
+You also need to set up a CORS policy for the bucket:
 
 ```json
 [
@@ -51,3 +51,25 @@ You also need to set up a CORS policy:
 	}
 ]
 ```
+
+You also need to set up a user with write access to your bucket. You can do this by creating a new user in the IAM console, and attaching the `AmazonS3FullAccess` policy to it. More granular access control policies are possible, but this is the simplest way to get started.
+
+When you paste an image from the clipboard into the Obsidian note, the plugin will upload the image to your bucket, and insert a link to the image in your note. The link will be of the form `https://<your-bucket>.s3.<your-region>.amazonaws.com/<your-optional-folder>/<image-name>`. If you have made your bucket world readable, you can share the link with others, and they will be able to view the image.
+
+If you select the "Upload on drag" option in the plugin settings, the plugin will also upload images that you drag into the note. This is useful if you want to upload images from your file system. If you do not want this behavior in all notes, you can enable it on a per note basis by adding a YAML frontmatter atg to the note, as seen below. You can also set the folder where the images will be uploaded to, by adding the `uploadFolder` option to the YAML frontmatter. These setting override the global settings.
+
+```
+---
+uploadOnDrag: true
+uploadFolder: "my-folder"
+---
+```
+
+## Development
+
+PR's are welcome, features that I would like to add include:
+
+-   [ ] Add support for other cloud storage providers, such as Google Drive, Dropbox, etc.
+-   [x] Add support for copying images to a configurable folder in the local file system, instead of uploading them to the cloud.
+-   [ ] Add support for dynamically moving images between the options above, through hotkeys.
+-   [ ] Add support for automatically creating buckets if they do not exist.
--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
-import * as mime from 'mime-types';
-import * as crypto from 'crypto';
-
 import {
+	App,
+	Editor,
 	Notice,
 	Plugin,
-	Editor,
-	MarkdownView,
+	PluginSettingTab,
+	Setting,
 	EditorPosition,
 } from "obsidian";
 
-  // Remember to rename these classes and interfaces!
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import * as mime from "mime-types";
+import * as crypto from "crypto";
+
+// Remember to rename these classes and interfaces!
 
 interface pasteFunction {
 	(this: HTMLElement, event: ClipboardEvent): void;
@@ -24,38 +25,45 @@ interface S3UploaderSettings {
 	bucket: string;
 	folder: string;
 	apiEndpoint: string;
-	uploadHeader: string;
-	uploadBody: string;
 	imageUrlPath: string;
-	maxWidth: number;
+	dragAndDrop: boolean;
+	localUpload: boolean;
+	localUploadFolder: string;
 }
 
 const DEFAULT_SETTINGS: S3UploaderSettings = {
-	accessKey: '',
-	secretKey: '',
-	region: '',
-	bucket: '',
-	folder: '',
-	apiEndpoint: null,
-	uploadHeader: null,
-	uploadBody: "{\"image\": \"$FILE\"}",
-	imageUrlPath: null,
-	maxWidth: 4096,
-}
+	accessKey: "",
+	secretKey: "",
+	region: "",
+	bucket: "",
+	folder: "",
+	apiEndpoint: "",
+	imageUrlPath: "",
+	dragAndDrop: true,
+	localUpload: false,
+	localUploadFolder: "",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: S3UploaderSettings;
 	s3: S3Client;
 	pasteFunction: pasteFunction;
 
-	private replaceText(editor: Editor, target: string, replacement: string): void {
-		target = target.trim()
+	private replaceText(
+		editor: Editor,
+		target: string,
+		replacement: string
+	): void {
+		target = target.trim();
 		const lines = editor.getValue().split("\n");
 		for (let i = 0; i < lines.length; i++) {
-			const ch = lines[i].indexOf(target)
+			const ch = lines[i].indexOf(target);
 			if (ch !== -1) {
 				const from = { line: i, ch: ch } as EditorPosition;
-				const to = { line: i, ch: ch + target.length } as EditorPosition;
+				const to = {
+					line: i,
+					ch: ch + target.length,
+				} as EditorPosition;
 				editor.setCursor(from);
 				editor.replaceRange(replacement, from, to);
 				break;
@@ -63,59 +71,113 @@ export default class MyPlugin extends Plugin {
 		}
 	}
 
-	async pasteHandler(ev: ClipboardEvent, editor: Editor, mkView: MarkdownView): Promise<void> {
+	async pasteHandler(
+		ev: ClipboardEvent | DragEvent,
+		editor: Editor
+	): Promise<void> {
 		if (ev.defaultPrevented) {
 			console.log("paste event is canceled");
 			return;
 		}
-	
-		let file = ev.clipboardData.files[0];
+
+		const noteFile = this.app.workspace.getActiveFile();
+
+		if (!noteFile || !noteFile.name) return;
+
+		const fm = this.app.metadataCache.getFileCache(noteFile).frontmatter;
+		const perDocumentUpload = fm && fm.uploadOnDrag;
+		const perDocumentUploadFolder = fm ? fm.uploadFolder : null;
+
+		let file = null;
+
+		if (ev.dataTransfer && ev.dataTransfer !== null) {
+			if (!this.settings.dragAndDrop && !perDocumentUpload) {
+				return;
+			}
+			file = ev.dataTransfer.files[0];
+		} else {
+			file = ev.clipboardData.files[0];
+		}
 
 		const imageType = /image.*/;
 		if (file.type.match(imageType)) {
-	
 			ev.preventDefault();
-	
+
 			// set the placeholder text
-			const buf = await file.arrayBuffer()
-			const digest = crypto.createHash('md5').update(new Uint8Array(buf)).digest("hex");
+			const buf = await file.arrayBuffer();
+			const digest = crypto
+				.createHash("md5")
+				.update(new Uint8Array(buf))
+				.digest("hex");
 			const contentType = mime.lookup(file.name);
-			const newFileName = digest+"."+mime.extension(contentType);
-			const pastePlaceText = `![uploading...](${newFileName})\n`
-			editor.replaceSelection(pastePlaceText)
-	
+			const newFileName = digest + "." + mime.extension(contentType);
+			const pastePlaceText = `![uploading...](${newFileName})\n`;
+			editor.replaceSelection(pastePlaceText);
+
 			// upload the image
-			const key = this.settings.folder ? "/" + newFileName : newFileName;
-			this.s3.send(new PutObjectCommand({
-				Bucket: this.settings.bucket,
-				Key: key,
-				Body: file,
-				ContentType: contentType,
-			})).then(res => {
-				const url = this.settings.imageUrlPath + key;
-				const imgMarkdownText = `![image](${url})`
-				this.replaceText(editor, pastePlaceText, imgMarkdownText)
-			}).catch(err => {
-				console.log(err);
-			});
+			const folder = perDocumentUploadFolder
+				? perDocumentUploadFolder
+				: this.settings.folder;
+			const key = folder ? folder + "/" + newFileName : newFileName;
+
+			if (!this.settings.localUpload) {
+				this.s3
+					.send(
+						new PutObjectCommand({
+							Bucket: this.settings.bucket,
+							Key: key,
+							Body: file,
+							ContentType: contentType,
+						})
+					)
+					.then((res) => {
+						const url = this.settings.imageUrlPath + key;
+						const imgMarkdownText = `![image](${url})`;
+						this.replaceText(
+							editor,
+							pastePlaceText,
+							imgMarkdownText
+						);
+						new Notice("Image uploaded to S3");
+					})
+					.catch((err) => {
+						console.log(err);
+						new Notice(
+							"Error uploading image to S3: " + err.message
+						);
+					});
+			} else {
+				const localUploadFolder = perDocumentUploadFolder
+					? perDocumentUploadFolder
+					: this.settings.localUploadFolder;
+				const localUploadPath = localUploadFolder
+					? localUploadFolder + "/" + newFileName
+					: newFileName;
+				await this.app.vault.adapter.mkdir(localUploadFolder);
+				this.app.vault.adapter
+					.writeBinary(localUploadPath, buf)
+					.then(() => {
+						const imgMarkdownText = `![image](${localUploadPath})`;
+						this.replaceText(
+							editor,
+							pastePlaceText,
+							imgMarkdownText
+						);
+						new Notice("Image uploaded to local folder");
+					})
+					.catch((err) => {
+						console.log(err);
+						new Notice(
+							"Error uploading image to local folder: " +
+								err.message
+						);
+					});
+			}
 		}
 	}
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new S3UploaderSettingTab(this.app, this));
@@ -134,36 +196,25 @@ export default class MyPlugin extends Plugin {
 		this.pasteFunction = this.pasteHandler.bind(this);
 
 		this.registerEvent(
-			this.app.workspace.on('editor-paste', this.pasteFunction)
+			this.app.workspace.on("editor-paste", this.pasteFunction)
+		);
+		this.registerEvent(
+			this.app.workspace.on("editor-drop", this.pasteFunction)
 		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Jamie!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -176,66 +227,112 @@ class S3UploaderSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for s3 Image Uploader.'});
+		containerEl.createEl("h2", { text: "Settings for S3 Image Uploader" });
 
 		new Setting(containerEl)
-			.setName('AWS Access Key')
-			.setDesc('aws access key')
-			.addText(text => text
-				.setPlaceholder('access key')
-				.setValue(this.plugin.settings.accessKey)
-				.onChange(async (value) => {
-					console.log('ID: ' + value);
-					this.plugin.settings.accessKey = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("AWS Access Key ID")
+			.setDesc("AWS access key ID for an S3 permissioned user")
+			.addText((text) =>
+				text
+					.setPlaceholder("access key")
+					.setValue(this.plugin.settings.accessKey)
+					.onChange(async (value) => {
+						this.plugin.settings.accessKey = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
 		new Setting(containerEl)
-		.setName('AWS Secret Key')
-		.setDesc('aws secret key')
-		.addText(text => text
-			.setPlaceholder('secret key')
-			.setValue(this.plugin.settings.secretKey)
-			.onChange(async (value) => {
-				console.log('Secret: ' + value);
-				this.plugin.settings.secretKey = value;
-				await this.plugin.saveSettings();
-			}));
+			.setName("AWS Secret Key")
+			.setDesc("AWS secret key for that user")
+			.addText((text) =>
+				text
+					.setPlaceholder("secret key")
+					.setValue(this.plugin.settings.secretKey)
+					.onChange(async (value) => {
+						this.plugin.settings.secretKey = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
 		new Setting(containerEl)
-		.setName('Region')
-		.setDesc('aws region')
-		.addText(text => text
-			.setPlaceholder('aws region')
-			.setValue(this.plugin.settings.region)
-			.onChange(async (value) => {
-				console.log('Region: ' + value);
-				this.plugin.settings.region = value;
-				await this.plugin.saveSettings();
-			}));
+			.setName("Region")
+			.setDesc("AWS region of the S3 bucket")
+			.addText((text) =>
+				text
+					.setPlaceholder("aws region")
+					.setValue(this.plugin.settings.region)
+					.onChange(async (value) => {
+						this.plugin.settings.region = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
 		new Setting(containerEl)
-		.setName('s3 Bucket')
-		.setDesc('s3 bucket name')
-		.addText(text => text
-			.setPlaceholder('bucket name')
-			.setValue(this.plugin.settings.bucket)
-			.onChange(async (value) => {
-				console.log('Bucket: ' + value);
-				this.plugin.settings.bucket = value;
-				await this.plugin.saveSettings();
-			}));
+			.setName("S3 Bucket")
+			.setDesc("S3 bucket name")
+			.addText((text) =>
+				text
+					.setPlaceholder("bucket name")
+					.setValue(this.plugin.settings.bucket)
+					.onChange(async (value) => {
+						this.plugin.settings.bucket = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
 		new Setting(containerEl)
-		.setName('bucket folder')
-		.setDesc('optional folder in s3 bucket')
-		.addText(text => text
-			.setPlaceholder('folder')
-			.setValue(this.plugin.settings.folder)
-			.onChange(async (value) => {
-				console.log('Folder: ' + value);
-				this.plugin.settings.folder = value;
-				await this.plugin.saveSettings();
-			}));
-		}
+			.setName("Bucket folder")
+			.setDesc("Optional folder in s3 bucket")
+			.addText((text) =>
+				text
+					.setPlaceholder("folder")
+					.setValue(this.plugin.settings.folder)
+					.onChange(async (value) => {
+						this.plugin.settings.folder = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Upload on drag")
+			.setDesc("Upload drag and drop images as well as pasted images.")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.dragAndDrop)
+					.onChange(async (value) => {
+						this.plugin.settings.dragAndDrop = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("Copy to local folder")
+			.setDesc("Copy images to local folder instead of s3.")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.localUpload)
+					.onChange(async (value) => {
+						this.plugin.settings.localUpload = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		new Setting(containerEl)
+			.setName("Local folder")
+			.setDesc("Local folder to save images, instead of s3")
+			.addText((text) =>
+				text
+					.setPlaceholder("folder")
+					.setValue(this.plugin.settings.localUploadFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.localUploadFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
 }
